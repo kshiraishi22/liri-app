a440914fa79dcf852ebe6e4d0137e7adb3ec7aef
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-24 15:27:07 -0500

    fixed error, changed process.argv from 2 to 3

--- liri.js
+++ liri.js
@@ -1,23 +1,26 @@
 // Dependencies and References
-const dotenv = require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
 const Spotify = require("node-spotify-api");
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify)
-let input = process.argv[2]
+let input = process.argv[3]
 
-spotify
+let spotifyFunction = spotify
   .search({ type: "track", query: input}).then(function (response) {
-    // console.log(response.tracks.items[0].external_urls.spotify);
-    // console.log(response.tracks.items[0].name);
-    // console.log(response.tracks.items[0].album.name);
-    // console.log(response.tracks.items[0].artists[0].name);
+    if( response.tracks.items.length ){
+      console.log(response.tracks.items[0].external_urls.spotify);
+      console.log(response.tracks.items[0].name);
+      console.log(response.tracks.items[0].album.name);
+      console.log(response.tracks.items[0].artists[0].name);
+    } else {
+      console.log("No matches for " + input);
+    }
   }).catch(function (error) {
     console.log(error);
   });
 
-  switch (process.arg[2]) {
+  switch (process.argv[2]) {
     case "concert-this":
       concertFunction;
       break;

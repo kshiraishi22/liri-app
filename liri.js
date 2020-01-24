e29de8a4c602c1c9a124fa47e413af4ed90df055
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-23 20:48:55 -0500

    added dependecies as variables and attained spotify client ID

--- liri.js
+++ liri.js
@@ -1,2 +1,10 @@
-var dotenv = require("dotenv").config();
-var keys = require("./keys.js");
+// Dependencies and References
+const dotenv = require("dotenv").config();
+const axios = require("axios");
+const moment = require("moment");
+const spotify = require("node-spotify-api");
+const keys = require("./keys.js");
+
+// Spotify API call
+axios.get(
+// need Spotify URL

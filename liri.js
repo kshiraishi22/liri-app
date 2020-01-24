a9eadb61b2faaf0d3f60f5d0daf4dd0c8960e8c1
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-24 15:11:46 -0500

    add switch and spotify call

--- liri.js
+++ liri.js
@@ -2,9 +2,37 @@
 const dotenv = require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
-const spotify = require("node-spotify-api");
+const Spotify = require("node-spotify-api");
 const keys = require("./keys.js");
+const spotify = new Spotify(keys.spotify)
+let input = process.argv[2]
 
-// Spotify API call
-axios.get(
-// need Spotify URL
+spotify
+  .search({ type: "track", query: input}).then(function (response) {
+    // console.log(response.tracks.items[0].external_urls.spotify);
+    // console.log(response.tracks.items[0].name);
+    // console.log(response.tracks.items[0].album.name);
+    // console.log(response.tracks.items[0].artists[0].name);
+  }).catch(function (error) {
+    console.log(error);
+  });
+
+  switch (process.arg[2]) {
+    case "concert-this":
+      concertFunction;
+      break;
+    case "spotify-this-song":
+      spotifyFunction;
+      break;
+    case "movie-this":
+      movieFunction;
+      break;
+    case "do-what-it-says":
+      liriFunction
+      break;
+    default:
+       console.log("Not working. Try again");
+
+  };
+
+    

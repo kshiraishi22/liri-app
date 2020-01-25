17bd21fb938990022dde89549148bf50044fcf2a
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-25 11:34:06 -0500

    OMDB API call completed

--- liri.js
+++ liri.js
@@ -6,20 +6,44 @@ const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify)
 let input = process.argv[3]
 
-let spotifyFunction = spotify
-  .search({ type: "track", query: input}).then(function (response) {
-    if( response.tracks.items.length ){
-      console.log(response.tracks.items[0].external_urls.spotify);
-      console.log(response.tracks.items[0].name);
-      console.log(response.tracks.items[0].album.name);
-      console.log(response.tracks.items[0].artists[0].name);
-    } else {
-      console.log("No matches for " + input);
-    }
+// Spotify function
+// let spotifyFunction = spotify
+//   .search({ type: "track", query: input}).then(function (response) {
+//     if( response.tracks.items.length ){
+//       console.log(response.tracks.items[0].external_urls.spotify);
+//       console.log(response.tracks.items[0].name);
+//       console.log(response.tracks.items[0].album.name);
+//       console.log(response.tracks.items[0].artists[0].name);
+//     } else {
+//       console.log("No matches for " + input);
+//     }
+//   }).catch(function (error) {
+//     console.log(error);
+//   });
+
+// Axios OMDB API
+const movieFunction = () => {
+  axios({
+  method: "GET",
+  url: `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
+})  
+  .then(function(response) {
+    console.log("Title: " + response.data.Title);  
+    console.log("Year Released: " + response.data.Released);  
+    console.log("imdbRating: " + response.data.imdbRating);  
+    console.log("Rotten Tomatoes rating: " + response.data.Ratings.Value);  
+    console.log("Country: " + response.data.Country);  
+    console.log("Language: " + response.data.Language);  
+    console.log("Plot: " + response.data.Plot);  
+    console.log("Actors: " + response.data.Actors);  
+
   }).catch(function (error) {
     console.log(error);
-  });
+  
+  })
+}
 
+// Switch function for all the commands
   switch (process.argv[2]) {
     case "concert-this":
       concertFunction;
@@ -28,7 +52,7 @@ let spotifyFunction = spotify
       spotifyFunction;
       break;
     case "movie-this":
-      movieFunction;
+      movieFunction();
       break;
     case "do-what-it-says":
       liriFunction

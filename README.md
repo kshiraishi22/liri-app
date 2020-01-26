ec46935286fc3c7c5c535071a1330c7a8ebc9403
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-25 22:39:45 -0500

    added tech used and instructions on readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# liri-node-app
+# LIRI Bot
+
+## App Overview
+
+## Instructions To Run App
+1. Right click on the folder, "liri-node-app" and select open terminal to use in the command line.
+2. Out of the four commands below, decide which one to use to get the information you want.
+  - concert-this
+  - spotify-this-song
+  - movie-this
+  - do-what-it-says
+3. Once user selects one of the four commands, type in the command line the following: node liri.js <command> "". Inside the quote will be the search input that corresponds to one of the 4 commands. For example, typing in "node liri.js concert-this "celine dion" will return the name of the concert venue, location of the concert and what day it will be for the celine dion concert. 
+## Screenshot of App
+
+## Technologies Used for App
+- Node.js
+- OMDB API
+- Node Spotify API
+- BandsInTown API
+- Axios
+- fs
+- Moment
+- DotEnv
+
+
--- liri.js
+++ liri.js
@@ -8,41 +8,41 @@ let input = process.argv[3];
 var fs = require("fs");
 
 // Spotify function
-let spotifyFunction = spotify
-  .search({ type: "track", query: input}).then(function (response) {
-    if( response.tracks.items.length ){
-      console.log("Artist Name: " + response.tracks.items[0].artists[0].name);
-      console.log("Song Name: " + response.tracks.items[0].name);
-      console.log("Album Name: " + response.tracks.items[0].album.name);
-      console.log("Preview Link on Spotify: " + response.tracks.items[0].external_urls.spotify);
-    } else {
-      console.log("No matches for " + input);
-    }
-  }).catch(function (error) {
-    console.log(error);
-  });
-
-// Axios OMDB API
-// const movieFunction = () => {
-//   axios({
-//   method: "GET",
-//   url: `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
-// })  
-//   .then(function(response) {
-//     console.log("Title: " + response.data.Title);  
-//     console.log("Year Released: " + response.data.Released);  
-//     console.log("imdbRating: " + response.data.imdbRating);  
-//     console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);  
-//     console.log("Country: " + response.data.Country);  
-//     console.log("Language: " + response.data.Language);  
-//     console.log("Plot: " + response.data.Plot);  
-//     console.log("Actors: " + response.data.Actors);  
-
+// let spotifyFunction = spotify
+//   .search({ type: "track", query: input}).then(function (response) {
+//     if( response.tracks.items.length ){
+//       console.log("Artist Name: " + response.tracks.items[0].artists[0].name);
+//       console.log("Song Name: " + response.tracks.items[0].name);
+//       console.log("Album Name: " + response.tracks.items[0].album.name);
+//       console.log("Preview Link on Spotify: " + response.tracks.items[0].external_urls.spotify);
+//     } else {
+//       console.log("No matches for " + input);
+//     }
 //   }).catch(function (error) {
 //     console.log(error);
+//   });
+
+// Axios OMDB API
+const movieFunction = () => {
+  axios({
+  method: "GET",
+  url: `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
+})  
+  .then(function(response) {
+    console.log("Title: " + response.data.Title);  
+    console.log("Year Released: " + response.data.Released);  
+    console.log("imdbRating: " + response.data.imdbRating);  
+    console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);  
+    console.log("Country: " + response.data.Country);  
+    console.log("Language: " + response.data.Language);  
+    console.log("Plot: " + response.data.Plot);  
+    console.log("Actors: " + response.data.Actors);  
+
+  }).catch(function (error) {
+    console.log(error);
   
-//   })
-// }
+  })
+}
 
   // Axios BandsInTown API 
   // const concertFunction = () => {
@@ -61,14 +61,14 @@ let spotifyFunction = spotify
   //   })
   // }
 
+const liriFunction = () => { 
 fs.readFile("random.txt", "utf8", function(error, data ) {
   if (error) {
     return console.log(error);
   }
-  
     console.log(data);
 })
-
+}
 // Switch function for all the commands
   switch (process.argv[2]) {
     case "concert-this":
@@ -78,10 +78,13 @@ fs.readFile("random.txt", "utf8", function(error, data ) {
       spotifyFunction;
       break;
     case "movie-this":
-      movieFunction();
+      if (input != "") {
+        movieFunction();
+      }
+      else movieFunction("Mr. Nobody");
       break;
     case "do-what-it-says":
-      liriFunction
+      liriFunction()
       break;
     default:
        console.log("Not working. Try again");
